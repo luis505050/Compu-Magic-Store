a631fca37e6f99da57e0c1c5a41c9e39fa57d181
Luis Garcia <luisferna267@gmail.com>
2025-06-20 22:41:06 -0500

    style and reactor : Se mejoraron aimaciones como el hover en el css y ademas de crear y agregar contenido basico en la parte de conocenos

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,18 @@
+import "../styles/about.css";
+
 const About = () => {
-  return <h1>Acerca de nosotros</h1>;
+  return (
+    <>
+      <h1>Acerca de nosotros</h1>
+      <div className="formulario">
+        <form action="" className="form-contac">
+          <h3>Contactenos</h3>
+          <p><i class="bi bi-chat-left-dots icon"></i></p>
+          <h4>+51 917 361 031 <title>Número de contacto</title></h4>
+        </form>
+      </div>
+    </>
+  )
 };
 
 export default About;
